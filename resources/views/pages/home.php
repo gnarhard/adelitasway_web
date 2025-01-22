344a2f17da4470ac0ab9f3780cb45f84c5ea9bb4
Grayson Erhard <develop@graysonerhard.com>
2025-01-22 12:51:41 -0700

    tour date text color update

--- resources/views/pages/home.php
+++ resources/views/pages/home.php
@@ -40,7 +40,7 @@
     <div class="container">
         <div class="row">
             <h2 class="text-center" style="font-size: 4rem;">TOUR</h2>
-            <script charset="utf-8" src="https://widget.bandsintown.com/main.min.js"></script><a class="bit-widget-initializer" data-artist-name="Adelitas Way" data-display-local-dates="false" data-display-past-dates="false" data-auto-style="false" data-text-color="#e8e8fe" data-link-color="#7BBEFD" data-background-color="rgba(0,0,0,0)" data-display-limit="15" data-display-start-time="false" data-link-text-color="#ebecee" data-display-lineup="false" data-display-play-my-city="true" data-separator-color="rgba(124,124,124,0.25)"></a>
+            <script charset="utf-8" src="https://widget.bandsintown.com/main.min.js"></script><a class="bit-widget-initializer" data-artist-name="Adelitas Way" data-display-local-dates="false" data-display-past-dates="false" data-auto-style="false" data-text-color="#f8f9fa" data-link-color="#7BBEFD" data-background-color="rgba(0,0,0,0)" data-display-limit="15" data-display-start-time="false" data-link-text-color="#ebecee" data-display-lineup="false" data-display-play-my-city="true" data-separator-color="rgba(124,124,124,0.25)"></a>
         </div>
     </div>
 </section>
