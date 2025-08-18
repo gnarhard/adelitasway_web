5c53848e005e67a9b18473fa86d74e97daac5ff3
Grayson Erhard <develop@graysonerhard.com>
2025-08-18 14:57:22 -0400

    feat: add google tag

--- resources/views/layout/app.php
+++ resources/views/layout/app.php
@@ -4,6 +4,10 @@
 <?php include 'head.php'; ?>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WBDKL43Q"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <?php include 'nav.php'; ?>
 
     <main>
