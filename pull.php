2a954cd4d5b580b585596ea51a6544f1973cad83
Grayson Erhard <develop@graysonerhard.com>
2026-02-16 19:25:09 -0700

    fix: move site into public/ directory

--- pull.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Use in the "Post-Receive URLs" section of your GitHub repo.
-if ($_POST['payload']) {
-    shell_exec('cd /home4/adelitas/public_html && git pull'); // Replace with your actual directory
-}
