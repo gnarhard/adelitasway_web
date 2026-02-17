7487259fadf467802ab750953ec29f6a7174f797
Grayson Erhard <develop@graysonerhard.com>
2026-02-16 19:26:36 -0700

    fix: fix image paths

--- public/resources/views/layout/head.php
+++ public/resources/views/layout/head.php
@@ -3,10 +3,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="/images/favicon/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/images/favicon/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="/images/favicon/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/public/images/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/public/images/favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/public/images/favicon/site.webmanifest">
     <meta name="theme-color" content="#7BBEFD">
 
     <title>Adelitas Way</title>
